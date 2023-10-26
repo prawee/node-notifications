98af126472a44fbf98053c096d2d5efd6e7c05a1
Prawee Wongsa <prawee@hotmail.com>
2023-10-26 18:17:44 +0700

    test send with device token #2

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
 
-console.log(deviceToken.device)
+// console.log(deviceToken.device)
+const payload = {
+    notification: {
+        title: 'My Notification #1',
+        body: 'Hi, this is notification #1' 
+    }
+}
+
+admin.messaging().sendToDevice(deviceToken.device, payload)
+    .then((response) => {
+        console.log('Sent is successfully.\n', response)
+    })
+    .catch((error) => {
+        console.log('Sent is failed.\n ', error)
+    })
